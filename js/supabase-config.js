3aec6b795a371d223f58ce36ea9db1fd12f6f6fb
princechakusa <chakusaprince@gmail.com>
2026-06-27 20:26:00 +0400

    untrack supabase-config.js and backup.sql from git index

--- js/supabase-config.js
+++
@@ -1,3 +0,0 @@
-// DO NOT COMMIT THIS FILE - add it to .gitignore
-window.SUPABASE_URL = 'https://gxgytumhknmnwspxjzxw.supabase.co';
-window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4Z3l0dW1oa25tbndzcHhqenh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNzMwNDUsImV4cCI6MjA5MzY0OTA0NX0.ddJhWdUy7JVrSfdaSK8a0On3zuwssY2H4DWsxBhgbJs';
